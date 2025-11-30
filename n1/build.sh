c115fdab263b9a13ad5116b43b5a459c8e48d7b1
nnttm1122 <63239891+nnttm1122@users.noreply.github.com>
2025-11-30 17:15:44 +0800

    Add Chinese localization for Nikki package

--- n1/build.sh
+++ n1/build.sh
@@ -26,6 +26,7 @@ PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-app-nikki"
+PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
 # PACKAGES="$PACKAGES openssh-sftp-server"
 # PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 # 文件管理器
